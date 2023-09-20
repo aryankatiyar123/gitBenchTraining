857d6f15cf9ce7e46840181899a84e6aa4e5d608
aryankatiyar123 <aryan.katiyar@nagarro.com>
2023-09-20 15:52:08 +0530

    First commit

---
+++ src/git/First.java
@@ -0,0 +1,9 @@
+package git;
+
+public class First 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("I am first program");
+	}
+}
---
+++ src/git/Second.java
@@ -0,0 +1,9 @@
+package git;
+
+public class Second 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("I am Second program");
+	}
+}
